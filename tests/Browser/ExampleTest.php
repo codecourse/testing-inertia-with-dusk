2f264490f4da9a9bf624b6df407e7168ce9977c0
Alex Garrett-Smith <alex@codecourse.com>
2024-12-09 12:41:03 +0000

    Episode 03

--- tests/Browser/ExampleTest.php
+++ tests/Browser/ExampleTest.php
@@ -5,6 +5,7 @@ use Laravel\Dusk\Browser;
 test('basic example', function () {
     $this->browse(function (Browser $browser) {
         $browser->visit('/')
+                ->pause(3000)
                 ->assertSee('Laravel');
     });
 });
